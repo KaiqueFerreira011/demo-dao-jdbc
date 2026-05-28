ca60a0986c08c097b76d8aa380a18f95bdeaed22
Kaique Ferreira <kaique.ferreirakiko99@gmail.com>
2026-05-28 17:08:57 -0300

    Seller entity class

--- src/Application/application.java
+++ src/Application/application.java
@@ -3,11 +3,15 @@ package Application;
 import java.sql.Connection;
 import db.DB;
 import entities.Department;
+import entities.Seller;
+import java.util.Date;
 
 public class application {
 
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        
+        System.out.println(seller);
     }
 }
