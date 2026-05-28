6145ed6426831d1767776d38f427bb491ed1eca4
Kaique Ferreira <kaique.ferreirakiko99@gmail.com>
2026-05-28 17:28:57 -0300

    SellerDaoJDBC and DaoFactory

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -4,9 +4,14 @@ import entities.Department;
 import java.util.List;
 
 public interface DepartmentDao {
+
     void insert(Department obj);
+
     void update(Department obj);
+
     void deleteById(Integer id);
+
     Department findById(Integer id);
+
     List<Department> findAll();
 }
